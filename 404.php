f4c95ba1e47e4ac718a80cfa1e30b6e302aa1700
dkobrenski <dave@bbmdesigns.com>
2015-09-05 16:02:07 -0400

    updated config to use new template and assets version-control location

--- 404.php
+++ 404.php
@@ -43,26 +43,6 @@ The page may have moved, or you may have stumbled upon an outdated link. In any 
 
 If you have found a link on the page that is broken and needs updating, feel free to [contact us](/contact). Thanks!";
 
-//swiftype.com
-
-/* $specialContentBlocks[1]["blockType"] = "basic";
-$specialContentBlocks[1]["isMarkdown"] = 0;
-$specialContentBlocks[1]["htmlData"] = '<input type="text" class="form-control st-default-search-input"><div class="st-search-container"></div>';
-
-$specialContentBlocks[2]["blockType"] = "code";
-$opts = array("language"=>"html");
-$specialContentBlocks[2]["optionsJson"] = json_encode($opts);
-
-$specialContentBlocks[2]["htmlData"] = "<script type=\"text/javascript\">
-  (function(w,d,t,u,n,s,e){w['SwiftypeObject']=n;w[n]=w[n]||function(){
-  (w[n].q=w[n].q||[]).push(arguments);};s=d.createElement(t);
-  e=d.getElementsByTagName(t)[0];s.async=1;s.src=u;e.parentNode.insertBefore(s,e);
-  })(window,document,'script','//s.swiftypecdn.com/install/v2/st.js','_st');
-
-  _st('install','Kfc9Xbfg44ujTLepyUsu','2.0.0');
-</script>";
-*/
-
 include($siteHeader);
 include($templatesDir . "/$templateClass.inc.php");
 include($siteFooter);	
--- _init.inc.php
+++ _init.inc.php
@@ -2,7 +2,12 @@
 session_start();
 include('config/_config.inc.php');
 
-if(getenv("BUGSNAG_CLIENT")) {
+/**
+ * Init & Authentication for the PUBLIC site files
+ * this script gets included at the top of any page, and loads all config vars in.
+*/
+
+if(getenv("BUGSNAG_CLIENT") !== false) {
 	$bugsnag = new Bugsnag_Client(getenv("BUGSNAG_CLIENT"));
 	$bugSnagScriptName = $_SERVER["SERVER_NAME"] . $_SERVER["SCRIPT_NAME"];
 	if($_SERVER["QUERY_STRING"]) $bugSnagScriptName .= "?" . $_SERVER["QUERY_STRING"];
@@ -19,12 +24,24 @@ if(getenv("BUGSNAG_CLIENT")) {
 	set_exception_handler(array($bugsnag, "exceptionHandler"));
 }
 
+//get the directory of the assets for this site's chosen template, as specified in dot env, or use default "assets" dir
+if(getenv("SITE_TEMPLATE") !== false) {
+	$publicAssetsDir = getenv("CMS_ADMIN_BASE") . "/site/" . getenv("SITE_TEMPLATE"); // <-- this looks like: "/config/site/holderness" - notice the leading slash
+	$testDir = __DIR__ . $siteTemplateDir;
+	if(!is_dir($testDir)) {
+		$publicAssetsDir = "/assets";
+	}
+} else {
+	$publicAssetsDir = "/assets";
+}
+
 //set some variables for the public page
-$publicAssetsDir = "/assets";
-$publicTemplatesDir = "/templates";
-$publicHeaderFile = "/header.inc.php";
+$publicTemplatesDir = "/templates"; //this should live inside the $publicAssetsDir
+$publicHeaderFile = "/header.inc.php"; //these should live inside the $publicTemplatesDir
 $publicFooterFile = "/footer.inc.php";
 
+//now we can save the include paths for the template files for use elsewhere
+//TO DO: make these globals instead of vars
 $templatesDir = __DIR__ . $publicAssetsDir . $publicTemplatesDir;
 $siteHeader = $templatesDir . $publicHeaderFile;
 $siteFooter = $templatesDir . $publicFooterFile;
